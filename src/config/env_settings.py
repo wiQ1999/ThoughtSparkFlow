d4d994c45330cad522ac7957878520b8b4579214
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2025-09-09 22:27:02 +0200

    feat: add `.env` settings

---
+++ src/config/env_settings.py
@@ -0,0 +1,16 @@
+from pydantic import AnyUrl, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class EnvSettings(BaseSettings):
+    wp_api_url: AnyUrl = Field(validation_alias="WP_API_URL")
+    wp_user: str = Field(validation_alias="WP_USER")
+    wp_password: str = Field(validation_alias="WP_PASSWORD")
+    openai_api_key: str = Field(validation_alias="OPENAI_API_KEY")
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        secrets_dir=None,
+        env_nested_delimiter="__",
+    )
