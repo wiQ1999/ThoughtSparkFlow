c510be794401d8eda9e41411905175fb325ac082
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2025-09-21 20:34:46 +0200

    feat: add ports and DTOs

---
+++ src/thoughtsparkflow/domain/__init__.py
@@ -0,0 +1,31 @@
+"""Ports subpackage for thoughtsparkflow."""
+
+from .ports import (
+    EditorResult,
+    TopicsRequest,
+    DraftCreationRequest,
+    MediaUploadRequest,
+    PostMediaUpdateRequest,
+    WPPort,
+    TopicsGenRequest,
+    TopicWithCategoryResult,
+    ContentGenRequest,
+    TextGenPort,
+    ImageRequest,
+    ImageGenPort
+)
+
+__all__ = [
+   "EditorResult",
+    "TopicsRequest",
+    "DraftCreationRequest",
+    "MediaUploadRequest",
+    "PostMediaUpdateRequest",
+    "WPPort",
+    "TopicsGenRequest",
+    "TopicWithCategoryResult",
+    "ContentGenRequest",
+    "TextGenPort",
+    "ImageRequest",
+    "ImageGenPort"
+]
---
+++ src/thoughtsparkflow/domain/ports.py
@@ -0,0 +1,60 @@
+from typing import Protocol, Iterable
+
+from pydantic import BaseModel, EmailStr
+
+class EditorResult(BaseModel):
+    id: int
+    email: EmailStr
+
+class TopicsRequest(BaseModel):
+    published_num: int
+    draft_num: int
+
+class DraftCreationRequest(BaseModel):
+    topic: str
+    content: str
+    category: str
+    editor_id: int
+
+class MediaUploadRequest(BaseModel):
+    data: bytes
+    topic: str
+    post_id: int
+
+class PostMediaUpdateRequest(BaseModel):
+    post_id: int
+    media_id: int
+
+class WPPort(Protocol):
+    def get_all_editors(self) -> Iterable[EditorResult]: ...
+    def get_all_categories(self) -> Iterable[str]: ...
+    def get_last_topics(self, request: TopicsRequest) -> Iterable[str]: ...
+    def create_draft_post(self, request: DraftCreationRequest) -> int: ...
+    def upload_media(self, request: MediaUploadRequest) -> int: ...
+    def update_post_with_media(self, request: PostMediaUpdateRequest) -> bool: ...
+
+class TopicsGenRequest(BaseModel):
+    last_topics: list[str]
+    categories: list[str]
+    propmpt_id: str
+
+class TopicWithCategoryResult(BaseModel):
+    topic: str
+    category: str
+
+class ContentGenRequest(BaseModel):
+    topic: str
+    style_descritpion: str
+    category: str
+    propmpt_id: str
+
+class TextGenPort(Protocol):
+    def generate_topics(self, request: TopicsGenRequest) -> Iterable[TopicWithCategoryResult]: ...
+    def generate_content(self, request: ContentGenRequest) -> str: ...
+
+class ImageRequest(BaseModel):
+    topic: str
+    propmpt_id: str
+
+class ImageGenPort(Protocol):
+    def generate_image(self, request: ImageRequest) -> bytes: ...
